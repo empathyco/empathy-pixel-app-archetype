acafcc3c7ff597375c968f909e6782539e919450
Víctor CG <victorcg@empathy.co>
2026-04-14 18:19:26 +0200

    feat: close empathy on pageView events

--- react/index.tsx
+++ react/index.tsx
@@ -3,8 +3,15 @@ import type { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView':
+    case 'vtex:pageView': {
+      const urlParams = new URLSearchParams(window.location.search)
+      const queryParam = urlParams.get('query')
+
+      if (!queryParam) {
+        (window as any).InterfaceX?.close()
+      }
       break
+    }
     default:
       break
   }
